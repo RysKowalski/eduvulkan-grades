916849e8d456d78254a514dd641160a67c309584
RysKowalski <ryskowalski0@gmail.com>
2025-12-02 08:57:05 +0100

    added file for types

---
+++ typing_stuff.py
@@ -0,0 +1,67 @@
+from typing import TypedDict, Optional
+
+
+class DateInfo(TypedDict):
+    Timestamp: int
+    Date: str
+    DateDisplay: str
+    Time: str
+
+
+class PersonInfo(TypedDict):
+    Id: int
+    Surname: str
+    Name: str
+    DisplayName: str
+
+
+class SubjectInfo(TypedDict):
+    Id: int
+    Key: str
+    Name: str
+    Kod: str
+    Position: int
+
+
+class CategoryInfo(TypedDict):
+    Id: int
+    Name: str
+    Code: str
+
+
+class ColumnInfo(TypedDict):
+    Id: int
+    Key: str
+    PeriodId: int
+    Name: str
+    Code: str
+    Group: str
+    Number: int
+    Color: int
+    Weight: int
+    Subject: SubjectInfo
+    Category: CategoryInfo
+
+
+class GradeItem(TypedDict):
+    Id: int
+    Key: str
+    PupilId: int
+    ContentRaw: str
+    Content: str
+    Comment: str
+    Value: int
+    Numerator: Optional[int]
+    Denominator: Optional[int]
+    DateCreated: DateInfo
+    CreatedAt: str
+    DateModify: DateInfo
+    ModifiedAt: str
+    Creator: PersonInfo
+    Modifier: PersonInfo
+    Column: ColumnInfo
+    CorrectedGrade: Optional[int]
+
+
+# The full JSON is a list of GradeItem:
+GradeList = list[GradeItem]
